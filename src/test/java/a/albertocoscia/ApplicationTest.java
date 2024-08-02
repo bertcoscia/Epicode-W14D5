55e114f232baa617087dffe46c3b69816ef93beb
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-02 09:10:37 +0200

    Initial commit

---
+++ src/test/java/a/albertocoscia/ApplicationTest.java
@@ -0,0 +1,13 @@
+package a.albertocoscia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
